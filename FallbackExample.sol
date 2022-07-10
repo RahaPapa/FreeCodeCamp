e811f42216c3e214bba33ae8b3e9926bb1d2b611
RahaPapa <105720648+RahaPapa@users.noreply.github.com>
2022-07-10 11:49:38 +0300

    Create FallbackExample.sol

---
+++ FallbackExample.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Idendifier: MIT 
+pragma solidity ^0.8.7;
+
+contract FallbackExample {
+    uint256 public result;
+
+    receive() external payable {
+        result = 1;
+    }
+    
+    fallback() external payable {
+   result = 2;
+    }
+}
